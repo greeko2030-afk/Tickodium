5c32a7f2093a7a1886cf6453f955daa90d3d3712
GreekoASC <greeko2030@gmail.com>
2026-08-06 15:30:11 +0600

    Update TickodiumClient.java

--- src/main/java/com/greeko/tickodium/TickodiumClient.java
+++ src/main/java/com/greeko/tickodium/TickodiumClient.java
@@ -1,1 +1,24 @@
+package com.greeko.tickodium;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
+
+@Environment(EnvType.CLIENT)
+public class TickodiumClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        Tickodium.LOGGER.info("[Greeko Company] Tickodium Client 1.21.x Initialized.");
+
+        if (FabricLoader.getInstance().isModLoaded("vulkanmod")) {
+            Tickodium.LOGGER.info("[Greeko Company] VulkanMod 1.21.x detected! Rendering synchronization active.");
+        } else if (FabricLoader.getInstance().isModLoaded("sodium")) {
+            Tickodium.LOGGER.info("[Greeko Company] Sodium 1.21.x detected! OpenGL synchronization active.");
+        } else {
+            Tickodium.LOGGER.info("[Greeko Company] Running on Standard 1.21.x Vanilla OpenGL pipeline.");
+        }
+    }
+}
 
