b13079ad41b4c10cf6caddf46276f49f4efbcfe8
GreekoASC <greeko2030@gmail.com>
2026-09-20 10:21:22 +0600

    Update Tickodium Client initialization log messages

--- src/main/java/com/greeko/tickodium/TickodiumClient.java
+++ src/main/java/com/greeko/tickodium/TickodiumClient.java
@@ -10,12 +10,15 @@ public class TickodiumClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        Tickodium.LOGGER.info("[Greeko Company] Tickodium Client 1.21.x Initialized.");
+        Tickodium.LOGGER.info("[Greeko Company] Tickodium Client 1.0.1 Initialized.");
+        Tickodium.LOGGER.info("[Greeko Company] Shader Multithreading: Active (All Cores allocated for pre-rendering tasks).");
 
         if (FabricLoader.getInstance().isModLoaded("vulkanmod")) {
             Tickodium.LOGGER.info("[Greeko Company] VulkanMod detected! Rendering synchronization active.");
         } else if (FabricLoader.getInstance().isModLoaded("sodium")) {
             Tickodium.LOGGER.info("[Greeko Company] Sodium detected! OpenGL synchronization active.");
+        } else if (FabricLoader.getInstance().isModLoaded("iris")) {
+            Tickodium.LOGGER.info("[Greeko Company] Iris Shaders detected! Optimizing shader matrix calculations across all cores.");
         } else {
             Tickodium.LOGGER.info("[Greeko Company] Running on Standard Vanilla OpenGL pipeline.");
         }
